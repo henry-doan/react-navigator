b5c08709083e0987578ce207b8d19753fc58171c
Henry Doan <henrydoan96@gmail.com>
2016-11-18 17:57:43 -0700

    MVP finished

--- MyScene.js
+++ MyScene.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { View, Text, Navigator } from 'react-native';
+import React, { Component, PropTypes } from 'react';
+import { View, Text, Navigator, TouchableHighlight, StyleSheet } from 'react-native';
 
 export default class MyScene extends Component {
 	static get defaultProps() {
@@ -10,9 +10,40 @@ export default class MyScene extends Component {
 
 	render() {
 		return (
-			<View>
+			<View style={styles.container}>				
 				<Text>Hi! My Name is {this.props.title}.</Text>
+				<TouchableHighlight onPress={ this.props.onForward}>
+					<Text>Tap me to load the next scene</Text>
+				</TouchableHighlight>
+				<TouchableHighlight onPress={ this.props.onBack}>
+					<Text>Tap me to go back</Text>
+				</TouchableHighlight>
 			</View>
 		)
 	}
 }
+
+MyScene.propTypes = {
+	title: PropTypes.string.isRequired,
+	onForward: PropTypes.func.isRequired,
+	onBack: PropTypes.func.isRequired,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
--- index.ios.js
+++ index.ios.js
@@ -3,6 +3,7 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Navigator,
   View
 } from 'react-native';
 
@@ -11,9 +12,28 @@ import MyScene from './MyScene';
 export default class ReactNavigator extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <MyScene />
-      </View>
+      <Navigator
+        initialRoute={{ title: 'My Initial Scene', index: 0 }}
+        renderScene={(route, navigator) => 
+          <MyScene 
+            title={route.title} 
+
+            onForward={ () => {
+              const nextIndex = route.index + 1;
+              navigator.push({
+                title: 'Scene ' + nextIndex,
+                index: nextIndex,
+              });
+            }}
+
+            onBack={() => {
+              if (route.index > 0) {
+                navigator.pop();
+              }
+            }}
+          />
+        }
+      />
     );
   }
 }
