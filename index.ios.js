6be96aa50cff7c3126099ef813a76447fb99c7d9
Henry Doan <henrydoan96@gmail.com>
2016-11-19 15:01:17 -0700

    finished

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
   Navigator,
   View
@@ -38,23 +37,5 @@ export default class ReactNavigator extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('ReactNavigator', () => ReactNavigator);
